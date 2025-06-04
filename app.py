9a029e78755109438ff0cd117f14449dfa6cf9cc
mBintangAs <mbintangassp@gmail.com>
2025-06-04 13:45:30 +0700

    update ui

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ app = Flask(__name__)
 @app.get('/')
 def home():
     return render_template('home.html')
+@app.get('/deteksi')
+def deteksi():
+    return render_template('deteksi.html')
 
 @app.get('/test')
 def test():
-    return render_template('test.html')
+    return render_template('predict.html')
 @app.post('/')
 def submit():
     # Load the model from a joblib file
@@ -22,9 +25,9 @@ def submit():
     form_data = request.form
     gender = float(form_data.get('gender'))
     age = float(form_data.get('age'))
-    hypertension = float(form_data.get('hypertension'))
-    heart_disease = float(form_data.get('heart_disease'))
-    ever_married = float(form_data.get('ever_married'))
+    hypertension = float(form_data.get('hypertension') or 0)
+    heart_disease = float(form_data.get('heart_disease') or 0)
+    ever_married = float(form_data.get('ever_married') or 0)
     work_type = float(form_data.get('work_type'))
     Residence_type = float(form_data.get('residence_type'))
     avg_glucose_level = float(form_data.get('avg_glucose_level'))
@@ -42,26 +45,22 @@ def submit():
     # Add prediction result to the context
 
     if prediction[0] == 1:
-        result = {
-            'status': '⚠️ Deteksi menunjukkan berpotensi stroke. ⚠️',
-            'recommendations': [
-                "Konsultasikan dengan dokter segera dan jaga pola hidup sehat!",
-                "Jaga pola makan sehat (rendah garam, tinggi serat).",
-                "Rutin berolahraga minimal 30 menit sehari.",
-                "Hindari merokok dan konsumsi alkohol.",
-                "Pantau tekanan darah dan kadar gula secara teratur."
-            ]
-        }
+        return redirect(url_for('positive'))
+        
     else:
-        result = {
-            'status': '✅ Tidak terdeteksi berpotensi stroke.',
-            'recommendations': [
-                "Tetap jaga pola hidup sehat!"
-            ]
-        }
+        return redirect(url_for('negative'))
+        
     # Handle form submission here
     flash(result)
+    print(result)
     return redirect(url_for('home'))
+
+@app.get('/negative')
+def negative():
+    return render_template('negative.html')
+@app.get('/positive')
+def positive():
+    return render_template('positive.html')
 if __name__ == '__main__':
     app.secret_key = os.urandom(24).hex()
     app.run(debug=True,port=3000)
